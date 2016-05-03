d4df8e1141d0d514268debc5d7929f89aea50b30
WeibaYeQiang <yqiang1000@163.com>
2016-05-03 17:37:33 +0800

    Initial Commit

---
+++ TCS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TCS
+//
+//  Created by WeibaYeQiang on 16/5/3.
+//  Copyright © 2016年 YQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TCS/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TCS
+//
+//  Created by WeibaYeQiang on 16/5/3.
+//  Copyright © 2016年 YQ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
